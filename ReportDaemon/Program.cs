643e1ef585456430c892e2cd3c0f46f3d90ca6dc
Sterling <sterlingforrest93@gmail.com>
2024-05-11 14:39:48 -0500

    Daemon functioning, pushing to server

--- ReportDaemon/Program.cs
+++ ReportDaemon/Program.cs
@@ -1,12 +1,39 @@
 using ReportDaemon;
+using ReportManager.Models.SettingsModels;
 using ReportManager.Services;
+using Serilog;
+using Serilog.Events;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Warning)
+    .WriteTo.File("logs/ReportDaemon_log_.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog();
+
+builder.Services.Configure<ConnectionSettings>(builder.Configuration.GetSection("ConnectionSettings"));
+builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JWTSettings"));
 builder.Services.AddSingleton<ConnectionService>();
-builder.Services.AddScoped<ReportManagementService>();
-builder.Services.AddScoped<FolderManagementService>();
-builder.Services.AddScoped<DatabaseService>();
-builder.Services.AddScoped<SharedService>();
-builder.Services.AddScoped<EmailService>();
-builder.Services.AddHostedService<Worker>();
+builder.Services.AddSingleton<AppDatabaseService>();
+builder.Services.AddSingleton<GroupManagementService>();
+builder.Services.AddSingleton<ReportManagementService>();
+builder.Services.AddSingleton<FolderManagementService>();
+builder.Services.AddSingleton<DatabaseService>();
+builder.Services.AddSingleton<SharedService>();
+builder.Services.AddSingleton<EmailService>();
+builder.Services.AddTransient<Worker>();
+builder.Services.AddAuthentication(Microsoft.AspNetCore.Authentication.Negotiate.NegotiateDefaults.AuthenticationScheme)
+    .AddNegotiate();
+
 var host = builder.Build();
-host.Run();
+using (var scope = host.Services.CreateScope())
+{
+    var worker = scope.ServiceProvider.GetRequiredService<Worker>();
+    await worker.TriggerWorker(CancellationToken.None);
+}
+
+await host.RunAsync();
--- ReportDaemon/obj/Debug/net8.0/ReportDaemon.AssemblyInfo.cs
+++ ReportDaemon/obj/Debug/net8.0/ReportDaemon.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40a1a8ce95d0a6b54a4defddc4790f02c6efc588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba73ac6bba0432c7da7da7ea6d768364de7a136")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
