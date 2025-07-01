9bd8c8e507960f55affbb01455d1f6c9ddbdcfb0
Silver <starksilver79@gmail.com>
2025-07-01 18:17:57 -0500

    Cleanup, appsettings for Railway

--- ReportDaemon/obj/Debug/net8.0/ReportDaemon.AssemblyInfo.cs
+++ ReportDaemon/obj/Debug/net8.0/ReportDaemon.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d8b46aedfd3133c60a87fafb25e89d2eb3068e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1eff76a83e80e15b18e7db746f7585315cd642d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReportDaemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
